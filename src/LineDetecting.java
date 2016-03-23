97c72c24e7a00cac04ad33f74acb738d2e554fd3
Boris <kozyrev.b@mail.ru>
2016-03-23 14:45:42 +0300

    comments

--- src/LineDetecting.java
+++ src/LineDetecting.java
@@ -4,36 +4,36 @@ import java.util.Scanner;
 
 public class LineDetecting {
 
-    private static ArrayList<Integer[]> points;
-    private static ArrayList<ArrayList<Integer>> pointsInLine;
-    private static ArrayList<Double> passedKoef;
+    private static ArrayList<Integer[]> points; //тут храним считанные из файла точки
+    private static ArrayList<ArrayList<Integer>> pointsInLine; //тут массив, каждый элемент которого - массив из точек на одной линии
+    private static ArrayList<Double> passedKoef; //храним тангенсы угла наклона (для дальнейшей оптимизации)
 
     public static void main(String[] args) {
         points = new ArrayList<>();
         pointsInLine = new ArrayList<>();
         passedKoef = new ArrayList<>();
-        readFile();
-        checkPoints();
-        printFile();
+        readFile(); //очевидно
+        checkPoints(); //ищем точки на одной линии
+        printFile(); //выводим результат в файлик
     }
 
     public static void checkPoints(){
-        for(int i = 0; i < points.size() - 3; i++){
+        for(int i = 0; i < points.size() - 3; i++){ //начинаем бежать по всем точкам, i - индекс точки
 
-            if(passedKoef.contains((double)points.get(i)[0] / points.get(i)[1]))
+            if(passedKoef.contains((double)points.get(i)[0] / points.get(i)[1])) //если такой тангенс угла наклона уже был, то го следующий
                 continue;
 
-            ArrayList<Integer> tmpPoints = new ArrayList<>();
-            tmpPoints.add(i);
-            passedKoef.add((double) points.get(i)[0] / points.get(i)[1]);
+            ArrayList<Integer> tmpPoints = new ArrayList<>(); //tmpPoints - временный массив, в который будем добавлять точки с таким же тангенсом, что и точка i
+            tmpPoints.add(i); //добавляем точку
+            passedKoef.add((double) points.get(i)[0] / points.get(i)[1]); //также сохраняем ее тангенс
 
-            for(int j = i + 1; j < points.size(); j++){
+            for(int j = i + 1; j < points.size(); j++){ //в остальном массиве ищем точки с таким же тангенсом
                 if((double)points.get(i)[0] / points.get(i)[1] == (double)points.get(j)[0] / points.get(j)[1]){
                     tmpPoints.add(j);
                 }
             }
 
-            if(tmpPoints.size() >= 4)
+            if(tmpPoints.size() >= 4) //если в итоге 4 и более точек, то добавляем куда следует
                 pointsInLine.add(tmpPoints);
         }
     }
@@ -44,7 +44,7 @@ public class LineDetecting {
 
             Scanner scanner = new Scanner(new File("C:\\Users\\Егор\\Desktop\\input.txt"));
 
-            while(scanner.hasNextInt()){
+            while(scanner.hasNextInt()){ //файл состоит из интовских чисел, идущих через пробел
                 int x = scanner.nextInt();
                 int y = scanner.nextInt();
                 points.add(new Integer[]{x, y});
@@ -59,7 +59,7 @@ public class LineDetecting {
         try {
             FileWriter writer = new FileWriter("C:\\Users\\Егор\\Desktop\\output.txt", false);
             for(int i = 0; i < pointsInLine.size(); i++){
-                String text = "Точка " + String.valueOf(i + 1);
+                String text = "Линия " + String.valueOf(i + 1);
                 writer.write(text);
                 writer.write("\r\n");
 
